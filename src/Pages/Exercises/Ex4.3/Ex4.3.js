2caa7d3a89c083d46dcc98ffedc6f7dc62eca23d
ccaampss <annacampsromero@gmail.com>
2023-04-12 19:38:55 +0300

    update II

--- src/Pages/Exercises/Ex4.3/Ex4.3.js
+++ src/Pages/Exercises/Ex4.3/Ex4.3.js
@@ -1,6 +1,37 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import './Carousel.css';
 
-const Ex4_3 = () => {
-    return (<p> </p>    );
+const Carousel = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNextClick = () => {
+    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+  };
+
+  const handlePrevClick = () => {
+    setCurrentImageIndex(
+      currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1
+    );
+  };
+
+  return (
+    <div className="carousel">
+      <div
+        className="carousel-images"
+        style={{
+          transform: `translateX(-${currentImageIndex * 100}%)`
+        }}
+      >
+        {/* {images.map((image, index) => (
+          <div key={index} className="carousel-image">
+            <img src={image} alt={`Carousel Image ${index}`} />
+          </div> */}
+        ))}
+      </div>
+      <button onClick={handlePrevClick}>Next</button>
+      <button onClick={handleNextClick}>Prev</button>
+    </div>
+  );
 };
-export default Ex4_3;
+
+export default Carousel;
