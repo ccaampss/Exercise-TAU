faac3aa862ef5227c7e7fe66efe7efe1d94f7f32
ccaampss <annacampsromero@gmail.com>
2023-04-12 21:47:44 +0300

    more pictures

--- src/Pages/Exercises/Ex4.3/Ex4.3.js
+++ src/Pages/Exercises/Ex4.3/Ex4.3.js
@@ -5,16 +5,21 @@ const images = [
   "https://picsum.photos/500/300?random=2",
   "https://picsum.photos/500/300?random=3",
   "https://picsum.photos/500/300?random=4",
-  "https://picsum.photos/500/300?random=5"
+  "https://picsum.photos/500/300?random=5",
+  "https://picsum.photos/500/300?random=6",
+  "https://picsum.photos/500/300?random=7",
+  "https://picsum.photos/500/300?random=8",
+  "https://picsum.photos/500/300?random=9",
+  "https://picsum.photos/500/300?random=10"
 ];
 
 const ImageCarousel = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const handleKeyDown = e => {
-    if (e.keyCode === 37) { // left arrow
+    if (e.keyCode === 37) { // left arrow on keyboard
       setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
-    } else if (e.keyCode === 39) { // right arrow
+    } else if (e.keyCode === 39) { // right arrow on keyboard
       setCurrentImageIndex((currentImageIndex + 1) % images.length);
     }
   };
@@ -25,5 +30,4 @@ const ImageCarousel = () => {
     </div>
   );
 };
-
 export default ImageCarousel;
