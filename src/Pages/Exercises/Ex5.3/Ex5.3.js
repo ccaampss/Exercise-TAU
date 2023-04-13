9dae80e94618811c9084a619d6236ae1905809f1
ccaampss <annacampsromero@gmail.com>
2023-04-13 07:25:17 +0300

    5.3. done

--- src/Pages/Exercises/Ex5.3/Ex5.3.js
+++ src/Pages/Exercises/Ex5.3/Ex5.3.js
@@ -1,10 +1,37 @@
-import ButtonGo from "../../../components/Buttons/Link/Link";
-import { TbBalloon } from "react-icons/tb";
+import {useState} from 'react';
+import { Drawer } from '@mui/material';
 
-const Ex5_3Page = () => {
+const images = [
+  "https://www.paturros.es/wp-content/uploads/2021/07/pato-goma-informatico.jpg",
+  "https://www.henryschein.es/Products/pato-goma-sonido-sherman-901-5506_600x600.jpg",
+  "https://www.paturros.es/wp-content/uploads/2021/04/pato-goma-amarillo.jpg",
+];
+
+export default function AccountMenu() {
+  const [state, setState] = useState(false)
+  const [image, setImage] = useState(images[0])
+
+  function cambiarEstado(newState) {
+    setState(newState)
+  }
+
   return (
-    <ButtonGo to="/" icon={<TbBalloon/>} />
-  );
-};
+    <div>
+    <img src={image}/>
 
-export default Ex5_3Page;
+    <button onClick={()=>cambiarEstado(true)}>
+      Open drawer
+    </button>
+
+<Drawer
+      anchor={'bottom'}
+      open={state}
+      onClose={()=>cambiarEstado(false)}
+    ><p onClick={()=>setImage(images[0])}>Photo 1</p>
+    <p onClick={()=>setImage(images[1])}>Photo 2</p>
+    <p onClick={()=>setImage(images[2])}>Photo 3</p>
+
+    </Drawer>
+    </div>
+  );
+}
